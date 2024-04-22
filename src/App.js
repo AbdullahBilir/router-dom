c600581f7335f096ee5e01040d2837b764d8f2b6
Abdullah Bilir <abdullahblrr@gmail.com>
2024-04-22 19:20:44 +0300

    dom-router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPages";
+import Root from "./routes/Root";
+import Contact from "./routes/Contact";
+import { loader as rootLoader } from "./routes/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="App">Hello Word</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/Root.jsx
@@ -0,0 +1,65 @@
+import { Link, NavLink, Outlet, useLoaderData, Form } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export default function Root() {
+  const { contacts } = useLoaderData();
+
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Rehber</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <Link to={"/"}>Ana Sayfa</Link>
+            {contacts.length ? (
+              <>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>İsim Yok</i>
+                      )}{" "}
+                      {contact.favorite && <span>★</span>}
+                    </Link>
+                  </li>
+                ))}
+              </>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
