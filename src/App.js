51756cd8e35502bbc08f2ddfc842bd309f35abef
Abdullah Bilir <abdullahblrr@gmail.com>
2024-04-24 14:30:07 +0300

    eleman ekleme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./ErrorPages";
 import Root from "./routes/Root";
-import Contact from "./routes/Contact";
-import { loader as rootLoader } from "./routes/Root";
+import Contact, { loader as contactLoader } from "./routes/Contact";
+import { loader as rootLoader, action as rootAction } from "./routes/Root";
+import EditContact, { action as editAction } from "./routes/Edit";
 
 const router = createBrowserRouter([
   {
@@ -10,10 +11,18 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
